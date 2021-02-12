0ef9d4c0de99665974fe63d961037e2e53bea1b5
Vafin Artem <a.vafin85@gmail.com>
2021-02-12 11:43:09 +0500

    добавил скрипт открытия закрытия попапа

--- source/js/script.js
+++ source/js/script.js
@@ -20,7 +20,36 @@
 })();
 
 
-//Galery
+// Open & Close Popup
+
+(() => {
+  const popupElement = document.querySelector(`.js-popup`);
+  const openPopupButtonElement = document.querySelector(`.js-open-popup`);
+  const closePopupButtonElement = popupElement.querySelector(`.js-close-popup`);
+
+  const ACTIVE_CLASS = `show`
+  const BODY_OVERFLOW_CLASS = `overflow-h`
+
+  const closePopup = (evt) => {
+    evt.preventDefault()
+    document.body.classList.remove(BODY_OVERFLOW_CLASS)
+    popupElement.classList.remove(ACTIVE_CLASS)
+
+    closePopupButtonElement.removeEventListener(`click`, closePopup)
+  }
+
+  const openPopup = (evt) => {
+    evt.preventDefault()
+    document.body.classList.add(BODY_OVERFLOW_CLASS)
+    popupElement.classList.add(ACTIVE_CLASS)
+
+    closePopupButtonElement.addEventListener(`click`, closePopup)
+  }
+
+  openPopupButtonElement.addEventListener(`click`, openPopup)
+})();
+
+// Galery
 
 (() => {
   const sliderNode = document.querySelector(`.js-slider`);
